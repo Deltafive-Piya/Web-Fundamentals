1b3e47d26903fe318a6e4260f6b3783b9cda7f3e
Deltafive-Piya <pierstoddard@gmail.com>
2023-06-13 18:36:08 -0400

    w2d3 addition

---
+++ schedule/morning routine.bat
@@ -0,0 +1,5 @@
+:: Execute this file in cmd-opens class schedule and chill music
+::update zoho sheet every class
+@echo off
+start "" ".\zohoSheet_webFundamentals.url"
+start "" "https://www.youtube.com/watch?v=CfPxlb8-ZQ0"
