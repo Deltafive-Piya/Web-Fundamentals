9b05e4ea308c837d5ab8711e05d3afead26bed4a
Deltafive-Piya <pierstoddard@gmail.com>
2023-06-21 12:42:23 -0400

    complete-buttonClickerHTML-CSS-JS

--- w3d3/script.js
+++ w3d3/script.js
@@ -1,3 +1,7 @@
+// include script tag in HTML
+// JS components:
+
+// 1.LogIn/LogOut toggler
 function logToggle() {
     var button = document.getElementById("logInOutBtn")
     //SET TO ===, NOT =
@@ -6,4 +10,15 @@ function logToggle() {
     } else {
         button.innerHTML = "In";
     }
+}
+
+// 2.Remove "Add definition" onclick
+function removeBtn () {
+    var button = document.getElementById("AddDefBtn");
+    button.remove();
+}
+
+// 3.AlertBox likeBtn onclick
+function likeAlert () {
+    alert("Ninja was liked")
 }
