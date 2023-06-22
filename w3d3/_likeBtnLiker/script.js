5e73245f6f36005401e326c834cd4278ba16c37c
Deltafive-Piya <pierstoddard@gmail.com>
2023-06-21 22:58:54 -0400

    likingLikes-counter1complete

---
+++ w3d3/_likeBtnLiker/script.js
@@ -0,0 +1,13 @@
+// incriment likes
+function voteUp(id) {
+    console.log("vote", id)
+    if (id === 1) {
+        document.getElementById("vote1").innerHTML++;
+    } else if (id === 2) {
+        document.getElementById("vote2").innerHTML++;
+    } else if (id === 3) {
+        document.getElementById("vote3").innerHTML++;
+    } else if (id === 4 ) {
+        document.getElementById("vote4").innerHTML++; 
+    } 
+}
